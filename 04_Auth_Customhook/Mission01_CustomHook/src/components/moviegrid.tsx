41a881e5c34cd885152348305125036759e37aea
goldjin06 <goldjin06@naver,com>
2026-04-12 11:32:33 +0900

    submit: 4주차 미션 1 제출

--- 04_Auth_Customhook/Mission01_CustomHook/src/components/moviegrid.tsx
+++ 04_Auth_Customhook/Mission01_CustomHook/src/components/moviegrid.tsx
@@ -1,51 +1,36 @@
-import { useEffect, useState } from "react";
 import type { ListType, Movie } from "../types/movie";
-import { getMovie } from "../utils/movieApi";
 import { MovieCard } from "./movieGridCard";
 import { usePage } from "../contexts/pageContext";
 import { LoadingSpinner } from "./loadingSpinner";
+import { useFetch } from "../hooks/useFetch";
+import { useEffect } from "react";
 
 const MovieGallary = ({type}: {type:ListType}) => {
     const {page, setMaxPage} = usePage();
-    const [movies, setMovies] = useState<Movie[]>([]);
-    const [isError, setIsError] = useState<boolean>(false);
-    const [isPending, setIsPending] = useState<boolean>(true);
+    const {data, isError, isPending} = useFetch<{ movies: Movie[], maxPage: number }>({dataType: "movie", page, type})
 
     useEffect(() => {
-        setIsError(false);
-        setIsPending(true);
-
-        const fetchMovies = async () => {
-            try {
-                const { data } = await getMovie(page, type)
-                setMovies(data.results);
-                setMaxPage(data.total_pages)
-            } catch {
-                setIsError(true);
-            } finally {
-                setIsPending(false);
-            }
-
-        };
-
-
-        fetchMovies();
+        if (data)
+            setMaxPage(data.maxPage)
+    },[data?.maxPage])
+    
         
-    }, [page, type]);
-
     if (isPending) {
         return (
             <LoadingSpinner />
         )
     }
 
-    if (isError) {
+    if (isError || !data) {
         return (
             <div>
                 <span>에러가 발생했습니다.</span>
             </div>
         )
     }
+
+    const movies: Movie[] = data.movies
+    
 
     return (
         <ul className="grid grid-cols-5 gap-3 content-center justify-center list-none w-[800px] p-0 m-0">
---
+++ 04_Auth_Customhook/Mission01_CustomHook/src/hooks/useFetch.tsx
@@ -0,0 +1,66 @@
+import { useEffect, useState } from "react"
+import { getMovie, getMovieCredit, getMovieInfo } from "../utils/movieApi";
+import type { CastCrew, ListType, Movie, MovieInfo } from "../types/movie";
+
+
+interface FetchParams {
+  dataType: "info" | "cast" | "movie";
+  movieId?: string;
+  page?: number;
+  type?: ListType;
+}
+
+export const useFetch = <T, >({ dataType, movieId, page, type }: FetchParams) => { // 제네릭과 HTML태그와의 모호함 해소를 위해 쉼표 추가
+    const [data, setData] = useState<T>()
+    const [isError, setIsError] = useState<boolean>(false);
+    const [isPending, setIsPending] = useState<boolean>(true);
+    
+    useEffect(() => {
+        setIsError(false);
+        setIsPending(true);
+
+        const fetchData = async () => {
+            try {
+                switch(dataType) {
+                    case "info": {
+                        if (!movieId) {
+                            throw new Error("movieId 없음");
+                        }
+                        const {data: result} = await getMovieInfo(movieId);
+                        setData(result as T); // as 진짜 넣기 싫었는데 switch문을 쓰면 타입 단언이 안되서 이렇게 해야한다고 하네요 다른 방법이 없을까요 ㅜ
+                        break;
+                    }
+                    case "cast": {
+                        if (!movieId) {
+                            throw new Error("movieId 없음");
+                        }
+                        const {data: result} = await getMovieCredit(movieId);
+                        setData([...result.cast, ...result.crew] as T)
+                        break;
+                    }
+                    case "movie": {
+                        if(!page || !type) {
+                            throw new Error("카테고리 type과 page 없음")
+                        }
+                        const {data: result} = await getMovie(page, type)
+                        setData({movies: result.results, maxPage: result.total_pages} as T);
+                        break;
+                    }
+                }
+            } catch {
+                setIsError(true);
+            } finally {
+                setIsPending(false);
+            }
+
+        };
+
+        fetchData();
+        
+    },[page, type]) // 제미나이 피셜 여기에 모든 파라미터를 넣으라는데,,, 왠지 진짜 모르겠음!!!
+
+    return(
+        {data, isError, isPending}
+    )
+    
+}
