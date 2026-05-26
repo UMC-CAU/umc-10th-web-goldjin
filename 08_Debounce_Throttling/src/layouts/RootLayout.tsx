99019dac9ffbf8ac7c8be06aaabe7b45dd1bf877
goldjin06 <goldjin06@naver,com>
2026-05-26 11:02:28 +0900

    8주차 미션 3

--- 08_Debounce_Throttling/src/layouts/RootLayout.tsx
+++ 08_Debounce_Throttling/src/layouts/RootLayout.tsx
@@ -2,15 +2,28 @@ import { Outlet } from 'react-router-dom';
 import { Navbar } from '../components/navbar';
 import { Sidebar } from '../components/sidebar';
 import { useEffect, useState } from 'react';
+import { useSidebar } from '../hooks/useSidebar';
+
 
 
 const RootLayout = () => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
   const [isMobile, setIsMobile] = useState(window.innerWidth < 1024);
-  const toggleSidebar = () => {
-    setIsSidebarOpen((prev) => !prev);
+  const {closeSidebar, toggleSidebar, isSidebarOpen} = useSidebar();
 
-  }
+  useEffect(() => {
+    if (!isSidebarOpen) return;
+
+    // 1. 현재 브라우저의 원래 overflow 스타일을 기억해둡니다.
+    const originalStyle = window.getComputedStyle(document.body).overflow;
+    
+    // 2. body의 스크롤을 막습니다.
+    document.body.style.overflow = "hidden";
+
+    // 3. 컴포넌트가 닫히거나 언마운트될 때 원래 스타일로 복구합니다. (클린업 함수)
+    return () => {
+      document.body.style.overflow = originalStyle;
+    };
+  }, [isSidebarOpen]);
 
 
 
@@ -22,7 +35,7 @@ const RootLayout = () => {
 
 
   if (isMobile) {
-    setIsSidebarOpen(false);
+    closeSidebar();
   }
 
   window.addEventListener('resize', handleResize);
@@ -48,7 +61,7 @@ const RootLayout = () => {
           {isMobile && isSidebarOpen && (
             <div
               className="fixed inset-0 bg-black/70 z-40"
-              onClick={toggleSidebar}
+              onClick={closeSidebar}
             />
           )}
         </div>  
